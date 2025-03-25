90a4dc8714f035ff32ea2238413af25ebcb3e7fe
adeiji <adeiji@yahoo.com>
2025-03-25 11:44:01 -0700

    Changed the structure to work better for scaling

--- types/customer/customers.go
+++ types/customer/customers.go
@@ -3,11 +3,8 @@ package customer
 import (
 	"context"
 	"fmt"
-	"log"
 
-	firebase "firebase.google.com/go"
 	"cloud.google.com/go/firestore"
-	"google.golang.org/api/option"
 )
 
 // FirebaseAPIClient defines a method for creating a customer in the database
@@ -48,27 +45,4 @@ func (h *FirebaseDBClient) AddCustomerToDatabase(ctx context.Context, customer m
     }
     fmt.Println("Customer created with ID:", docRef.ID)
     return docRef.ID, nil
-}
-
-// NewFirebaseClient initializes Firebase and Firestore using a service account JSON file
-func NewFirebaseClient() *FirebaseDBClient {
-    ctx := context.Background()
-    fmt.Println("Starting Firebase initialization")
-    
-    // Initialize Firebase Admin SDK with credentials
-    opt := option.WithCredentialsFile("./service-account.json")
-    app, err := firebase.NewApp(ctx, nil, opt)
-    
-    if err != nil {
-        log.Fatalf("Error initializing Firebase: %v", err)
-    } else {
-        fmt.Println("Firebase initialized successfully")
-    }
-
-    client, err := app.Firestore(ctx)
-    if err != nil {
-        log.Fatalf("Error initializing Firestore: %v", err)
-    }
-
-    return &FirebaseDBClient{DB: client}
 }
